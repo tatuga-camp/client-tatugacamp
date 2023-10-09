070918e7a1adc978d04a8b596dedd2fe822f236e
permlap-phola <permlap@tatugacamp.com>
2023-10-09 17:36:27 +0700

    finish

--- service/student/attendance.js
+++ service/student/attendance.js
@@ -21,3 +21,83 @@ export async function GetAttendances({ studentId, classroomId }) {
     throw new Error(err);
   }
 }
+
+export async function ReadQrCodeAttendance({
+  attendanceQRCodeId,
+  classroomId,
+}) {
+  try {
+    const attendances = await axios.get(
+      `${process.env.NEXT_PUBLIC_SERVER_STUDENT_URL}/student/attendance/get/qr-code`,
+      {
+        params: {
+          attendanceQRCodeId: attendanceQRCodeId,
+          classroomId: classroomId,
+        },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+
+    return attendances.data;
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+}
+
+export async function UpdateQrCodeAttendance({
+  studentId,
+  attendanceId,
+  attendanceQRCodeId,
+  attendance,
+}) {
+  try {
+    const getLocalStoreOnattendanceQRCodeId =
+      localStorage.getItem(attendanceQRCodeId);
+
+    console.log(getLocalStoreOnattendanceQRCodeId);
+    if (getLocalStoreOnattendanceQRCodeId === null) {
+      const attendances = await axios.put(
+        `${process.env.NEXT_PUBLIC_SERVER_STUDENT_URL}/student/attendance/update`,
+        {
+          studentId,
+          attendanceId,
+          attendance,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      localStorage.setItem(`${attendanceQRCodeId}`, attendanceQRCodeId);
+
+      return attendances.data;
+    } else {
+      const attendances = await axios.put(
+        `${process.env.NEXT_PUBLIC_SERVER_STUDENT_URL}/student/attendance/update`,
+        {
+          studentId,
+          attendanceId,
+          attendanceQRCodeId: getLocalStoreOnattendanceQRCodeId,
+          attendance,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      localStorage.setItem(`${attendanceQRCodeId}`, attendanceQRCodeId);
+
+      return attendances.data;
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+}
