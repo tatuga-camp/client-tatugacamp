ca1f4df75ffaecde9272c49b11d5b4485090c274
permlap-phola <permlap@tatugacamp.com>
2023-11-06 09:55:09 +0700

    Bug: appearance-none on input checkbox is disable

--- components/form/assignMultipleClassroom.js
+++ components/form/assignMultipleClassroom.js
@@ -118,7 +118,7 @@ function AssignMultipleClassroom({ user, setTriggerAssignMultipleClassroom }) {
                         handleChangeCheckbox({ classroomId: classroom?.id })
                       }
                       type="checkbox"
-                      className="w-6 h-6 ring-2 appearance-none ring-black  text-blue-600 bg-gray-100 border-gray-300 rounded
+                      className="w-6 h-6 ring-2  ring-black  text-blue-600 bg-gray-100 border-gray-300 rounded
                      focus:ring-blue-500 dark:focus:ring-blue-600
                      dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
