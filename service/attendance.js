d1bb839d27decf50a431b56a4c3df318a657bf06
permlap-phola <permlap@tatugacamp.com>
2023-11-07 22:19:30 +0700

    update headAttendance note

--- service/attendance.js
+++ service/attendance.js
@@ -164,6 +164,106 @@ export async function GetAllAttendance({ classroomId }) {
   }
 }
 
+export async function UpdateHeadAttendance({
+  imagesBase64,
+  note,
+  headAttendanceId,
+  classroomId,
+}) {
+  try {
+    const cookies = parseCookies();
+    const access_token = cookies.access_token;
+    const formData = new FormData();
+
+    if (imagesBase64.length > 0) {
+      for (const imageBase64 of imagesBase64) {
+        const response = await fetch(imageBase64);
+        const blob = await response.blob();
+        const file = new File([blob], 'image.jpg', { type: 'image/jpeg' });
+        formData.append('files', file);
+      }
+      const filesOld = formData.getAll('files');
+      const files = await Promise.all(
+        filesOld.map(async (file) => {
+          if (file.type === '') {
+            const blob = await heic2any({
+              blob: file,
+              toType: 'image/jpeg',
+            });
+            file = new File([blob], file.name, { type: 'image/jpeg' });
+            return {
+              file: file,
+              fileName: file.name,
+              fileType: file.type,
+            };
+          } else {
+            return {
+              file: file,
+              fileName: file.name,
+              fileType: file.type,
+            };
+          }
+        }),
+      );
+
+      const urls = await axios.post(
+        `${process.env.Server_Url}/user/attendance/head-attendance/upload-signUrl`,
+        {
+          files,
+          classroomId,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${access_token}`,
+          },
+        },
+      );
+      for (let i = 0; i < urls.data.urls.length; i++) {
+        await fetch(urls.data.urls[i].SignedURL, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': `${urls.data.urls[i].contentType}`,
+          },
+          body: files[i].file,
+        }).catch((err) => console.log(err));
+      }
+
+      let updatedContent = note;
+      for (let i = 0; i < imagesBase64.length; i++) {
+        const base64Image = imagesBase64[i];
+        const imageUrl = urls.data.baseUrls[i];
+        updatedContent = updatedContent.replace(base64Image, imageUrl);
+      }
+
+      const update = await axios.put(
+        `${process.env.Server_Url}/user/attendance/head-attendance/update`,
+        { headAttendanceId, note: updatedContent },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        },
+      );
+      return update;
+    } else {
+      const update = await axios.put(
+        `${process.env.Server_Url}/user/attendance/head-attendance/update`,
+        { headAttendanceId, note },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        },
+      );
+      return update;
+    }
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
+}
+
 export async function DeleteAttendance({ groupId }) {
   try {
     const cookies = parseCookies();
