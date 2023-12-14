da18e1843bda793d0ceb7983a17c553ec3c9e519
Thunpitcha Rattantamjaroen <maz.tza69@gmail.com>
2023-12-14 20:21:29 +0700

    task1_14/12/66
    create layout with mockup photo

---
+++ components/card-aboutus/MockImg.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MockImg() {
+  return (
+    <div>
+      <div style={{ width: '400px', height: '400px' }} className='bg-slate-400'>
+               Mockup Image
+      </div>
+    </div>
+  )
+}
+
+export default MockImg
