a119b2954953e8f46dd4ee126a0fd57bbc0e2529
Thunpitcha Rattantamjaroen <maz.tza69@gmail.com>
2024-04-10 20:29:59 +0700

    update type processAttendanceMonthly

--- utils/processAttendanceData.ts
+++ utils/processAttendanceData.ts
@@ -1,7 +1,30 @@
-import useAttendanceData from "../../../pages/classroom/teacher/[classroomId]/attendance/index";
+import { UseQueryResult } from "@tanstack/react-query";
+import useAttendanceData from "../pages/classroom/teacher/[classroomId]/attendance/index"
+import { ResponseGetAllAttendanceService } from "@/services/attendance";
 
-export function processAttendanceData(attendances) {
-  const yearCounts = {};
+interface StatusCount {
+  statCount: number;
+  months: number[];
+}
+
+interface YearStatuses {
+  [status: string]: StatusCount;
+}
+
+interface YearCount {
+  count: number;
+  statuses: YearStatuses;
+}
+
+interface YearCounts {
+  [year: number]: YearCount;
+}
+
+export function processAttendanceData(
+  attendances: UseQueryResult<ResponseGetAllAttendanceService, Error>
+) {
+  console.log(attendances);
+  const yearCounts: YearCounts = {};
 
   const statusTypeArray = [
     "absent",
@@ -17,24 +40,28 @@ export function processAttendanceData(attendances) {
   attendances?.data?.students.forEach((item, index) => {
     if (index + 1 !== 0) {
       item.data.forEach((attendance) => {
+        //console.log(item);
         const year = new Date(attendance.date).getFullYear();
         const month = new Date(attendance.date).getMonth();
 
         yearCounts[year] = yearCounts[year] || { count: 0, statuses: {} };
         yearCounts[year].count += 1;
 
-        statusTypeArray.forEach((status) => {
+        statusTypeArray.forEach((status: string) => {
           yearCounts[year].statuses[status] = yearCounts[year].statuses[
             status
           ] || { statCount: 0, months: monthsArray.slice() };
-          yearCounts[year].statuses[status].statCount += attendance[status]
+          yearCounts[year].statuses[status].statCount += attendance[
+            status as keyof typeof attendance
+          ]
             ? 1
             : 0;
 
-          if (attendance[status]) {
+          if (attendance[status as keyof typeof attendance]) {
             yearCounts[year].statuses[status].months[month]++;
           }
         });
+        //console.log(yearCounts);
       });
     }
   });
