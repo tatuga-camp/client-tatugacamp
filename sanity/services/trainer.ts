f5bce104ab911b488213fbe966742bfd0ad1ad7d
Permlap Phola <permlap@tatugacamp.com>
2026-07-02 17:06:21 +0700

    fix: skip trainer documents published without a photo

--- sanity/services/trainer.ts
+++ sanity/services/trainer.ts
@@ -5,7 +5,7 @@ type ResponseGetTrainersSanityService = Trainer[];
 
 export async function GetTrainersSanityService(): Promise<ResponseGetTrainersSanityService> {
   try {
-    const query = `*[_type == "trainer"] | order(_createdAt asc){
+    const query = `*[_type == "trainer" && defined(mainImage.asset)] | order(_createdAt asc){
         mainImage{
             asset->{
                     url,
